cdc2661560443d7cf4734a7a738411132931508c
Greg Rapp <gdrapp@gmail.com>
2012-04-28 10:03:33 -0400

    Added code to AppleApnsServiceImpl to shutdown APNs inactive device service when container is shutdown.

--- src/main/java/net/gregrapp/jhouse/services/AppleApnsServiceImpl.java
+++ src/main/java/net/gregrapp/jhouse/services/AppleApnsServiceImpl.java
@@ -74,6 +74,7 @@ public class AppleApnsServiceImpl implements AppleApnsService
   {
     logger.info("Destroying Apple Push Notification service");
     apnsService.stop();
+    inactiveDeviceExecutor.shutdownNow();
   }
 
   /**
