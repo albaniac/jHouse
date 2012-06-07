6570f8f6d8fb7674e709a000341c3718755471ce
Greg Rapp <gdrapp@gmail.com>
2012-06-06 23:20:34 -0400

    Fixed bug where UserLocationRepository getNewestUserLocation returned multiple locations for a user if they have two locations with the same timestamp

--- src/main/java/net/gregrapp/jhouse/repositories/UserLocationRepositoryImpl.java
+++ src/main/java/net/gregrapp/jhouse/repositories/UserLocationRepositoryImpl.java
@@ -37,7 +37,8 @@ public class UserLocationRepositoryImpl implements UserLocationRepositoryCustom
   @Override
   public List<UserLocation> getNewestLocationForEachUser()
   {
-    Query query = em.createNativeQuery("SELECT ul.* FROM user_locations ul JOIN (SELECT user_id,MAX(timestamp) AS timestamp FROM user_locations GROUP BY user_id) ul2 ON ul.user_id = ul2.user_id AND ul.timestamp = ul2.timestamp", UserLocation.class);
+    //Query query = em.createNativeQuery("SELECT ul.* FROM user_locations ul JOIN (SELECT user_id,MAX(timestamp) AS timestamp FROM user_locations GROUP BY user_id) ul2 ON ul.user_id = ul2.user_id AND ul.timestamp = ul2.timestamp", UserLocation.class);
+    Query query = em.createNativeQuery("SELECT ul.* FROM user_locations ul JOIN (SELECT MAX(ul1.id) as id,ul1.user_id FROM user_locations ul1 JOIN (SELECT user_id,MAX(timestamp) AS timestamp FROM user_locations GROUP BY user_id) ul2 ON ul1.user_id = ul2.user_id AND ul1.timestamp = ul2.timestamp GROUP BY user_id) new ON ul.id=new.id", UserLocation.class);
     @SuppressWarnings("unchecked")
     List<UserLocation> results = query.getResultList();
     return results;
